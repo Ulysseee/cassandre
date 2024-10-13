c06e4f8c33daadf8d6cc5ecd08a2b0c037d72c9e
Ulysseee <gravier.ulysse@gmail.com>
2024-10-13 20:41:47 +0200

    🔧 scss sections & parallax

--- app/components/Parallax.js
+++ app/components/Parallax.js
@@ -1,40 +1,50 @@
-import { Base, withFreezedOptions, withScrolledInView } from '@studiometa/js-toolkit';
+import {
+  Base,
+  withFreezedOptions,
+  withScrolledInView,
+} from "@studiometa/js-toolkit";
 
-export default class Parallax extends withFreezedOptions(withScrolledInView(Base)) {
+export default class Parallax extends withFreezedOptions(
+  withScrolledInView(Base)
+) {
+  static config = {
+    name: "Parallax",
+    options: {
+      percent: {
+        type: Number,
+        default: 10,
+      },
+      reverse: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    refs: ["container", "image"],
+  };
 
-    static config = {
-        name: 'Parallax',
-        options: {
-            percent: {
-                type: Number,
-                default: 10,
-            },
-            reverse: {
-                type: Boolean,
-                default: false,
-            },
-        },
-        refs: ['container', 'image'],
+  scrollProgressY = 0;
+
+  mounted() {
+    this.$refs.image.style.willChange = "transform";
+    this.scaleString = `scale(${1 + this.$options.percent / 100}, ${
+      1 + this.$options.percent / 100
+    })`;
+    this.$el.style.transform = `scaleX(${1 + this.$options.percent / 100})`;
+    this.$refs.container.style.transform = this.scaleString;
+  }
+
+  scrolledInView({ progress }) {
+    this.scrollProgressY = progress.y - 0.5;
+  }
+
+  ticked() {
+    const percentY =
+      this.scrollProgressY *
+      this.$options.percent *
+      (this.$options.reverse ? 1 : -1);
+
+    return () => {
+      this.$refs.container.style.transform = `${this.scaleString} translateY(${percentY}%)`;
     };
-
-    scrollProgressY = 0;
-
-    mounted () {
-        this.$refs.image.style.willChange = 'transform';
-        this.scaleString = `scale(${ 1 - this.$options.percent / 100 }, ${ 1 + this.$options.percent / 100 })`;
-        this.$el.style.transform = `scaleX(${ 1 + this.$options.percent / 100 })`;
-        this.$refs.container.style.transform = this.scaleString
-    }
-
-    scrolledInView ({ progress }) {
-        this.scrollProgressY = progress.y - 0.5;
-    }
-
-    ticked () {
-        const percentY = this.scrollProgressY * this.$options.percent * (this.$options.reverse ? 1 : -1);
-
-        return () => {
-            this.$refs.container.style.transform = `${this.scaleString} translateY(${ percentY }%)`;
-        };
-    }
+  }
 }
