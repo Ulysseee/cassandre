14422f1ffb788de673aae6983aaa2a2d82fc4e6f
Ulysseee <gravier.ulysse@gmail.com>
2024-10-14 14:24:23 +0200

    🔧 project page

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,12 +1,7 @@
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 import Page from "../containers/Page";
 import Work from "../components/Work";
 import Footer from "../components/Footer";
 import HomeHeader from "../components/HomeHeader";
-
-gsap.registerPlugin(ScrollTrigger);
 
 export default class Home extends Page {
   static config = {
@@ -22,31 +17,6 @@ export default class Home extends Page {
 
   mounted() {
     super.mounted();
-
-    this.boot();
-  }
-
-  boot() {
-    // let tl = gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: this.$el,
-    //     // start: "top top", // when the top of the trigger hits the top of the viewport
-    //     start: `${window.innerHeight}`,
-    //     end: `bottom-=30% bottom`, // end after scrolling 500px beyond the start
-    //     scrub: 1,
-    //     // markers: true,
-    //     // onEnter: () => {
-    //     //   console.log("Enter");
-    //     // },
-    //     // onLeave: () => {
-    //     //   console.log("Leave");
-    //     // },
-    //     // onEnterBack: () => {
-    //     //   console.log("EnterBack");
-    //     // },
-    //   },
-    // });
-    // tl.to(this.$el, { backgroundColor: "#ede9e3" });
   }
 
   destroyed() {
--- app/pages/Projects.js
+++ app/pages/Projects.js
@@ -1,8 +1,12 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Page from "../containers/Page";
 import Footer from "../components/Footer";
 import Paragraph from "../components/Paragraph";
 import Title from "../components/Title";
 import SVGReveal from "../components/SVGReveal";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default class Projects extends Page {
   static config = {
@@ -22,6 +26,22 @@ export default class Projects extends Page {
     [this.title] = this.$children.Title;
     [this.scribble] = this.$children.SVGReveal;
     this.title.onAnimateInComplete = this.onTitleAnimateInStart.bind(this);
+
+    this.boot();
+  }
+
+  boot() {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.$el,
+        // start: "top top", // when the top of the trigger hits the top of the viewport
+        start: `${window.innerHeight}`,
+        end: `bottom-=50% bottom`, // end after scrolling 500px beyond the start
+        scrub: 1,
+        // markers: true,
+      },
+    });
+    tl.to(this.$el, { backgroundColor: "#ede9e3" });
   }
 
   onTitleAnimateInStart() {
