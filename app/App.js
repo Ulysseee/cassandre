ca4ae574422cca2e5d620e0269b63d5a4af6b020
Ulysseee <gravier.ulysse@gmail.com>
2024-10-14 10:50:31 +0200

    🔧 last commit related

--- app/App.js
+++ app/App.js
@@ -170,4 +170,4 @@ const appLoaded = new Promise((resolve) => {
 });
 
 Promise.all([appLoaded, overlayTransition.animateIn()]).then(bootApp);
-Promise.all([appLoaded]).then(bootApp);
+// Promise.all([appLoaded]).then(bootApp);
