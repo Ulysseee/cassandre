e288b79bac5b978bcd262fe3b48ecee8c7885217
Ulysseee <gravier.ulysse@gmail.com>
2024-10-14 10:20:20 +0200

    🔧 nextProject

--- app/containers/AppEvents.js
+++ app/containers/AppEvents.js
@@ -9,13 +9,9 @@ export default class AppEvents extends Base {
 
   cursor;
 
-  mounted() {
-    console.log("Mounted");
-    console.log("this.$refs", this.$refs);
-  }
+  mounted() {}
 
   onCursorLinkMouseenter(e) {
-    console.log("onCursorLinkMouseenter");
     const cursorElement = document.querySelector('[data-component="Cursor"]');
     this.cursor = getInstanceFromElement(cursorElement, Cursor);
     if (this.cursor && this.cursor.$isMounted) this.cursor.onEnterLink(e);
