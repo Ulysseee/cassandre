6666605dae21a1e06dfdbcf8e2f69419287449fb
Ulysseee <gravier.ulysse@gmail.com>
2024-10-14 10:48:56 +0200

    🔧 set transitions back

--- app/App.js
+++ app/App.js
@@ -165,9 +165,9 @@ const bootApp = async () => {
 const appLoaded = new Promise((resolve) => {
   window.addEventListener("load", resolve);
   window.onbeforeunload = function () {
-    // window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
   };
 });
 
-// Promise.all([appLoaded, overlayTransition.animateIn()]).then(bootApp);
+Promise.all([appLoaded, overlayTransition.animateIn()]).then(bootApp);
 Promise.all([appLoaded]).then(bootApp);
